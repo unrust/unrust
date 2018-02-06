7f215ff65c42fcd399118e97d624913ce73f44b3
Edwin Cheng <edwin0cheng@gmail.com>
2018-02-07 01:33:17 +0800

    Make web_fs fail nice when fail to load from web.

--- uni-app/src/web_fs.rs
+++ uni-app/src/web_fs.rs
@@ -2,20 +2,42 @@ use std;
 use stdweb::web::TypedArray;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::io::{Error, ErrorKind};
 
 pub type IoError = std::io::Error;
+
 pub struct FileSystem {}
+
+enum BufferState {
+    Empty,
+    Buffer(Vec<u8>),
+    Error(String),
+}
+
 pub struct File {
-    buffer: Rc<RefCell<Vec<u8>>>,
+    buffer_state: Rc<RefCell<BufferState>>,
 }
 
 impl FileSystem {
     pub fn open(s: &str) -> Result<File, IoError> {
-        let buffer = Rc::new(RefCell::new(vec![]));
-        let buffer_p = buffer.clone();
+        let buffer_state = Rc::new(RefCell::new(BufferState::Empty));
 
-        let get_buffer = move |ab: TypedArray<u8>| {
-            *buffer_p.borrow_mut() = ab.to_vec();
+        let on_get_buffer = {
+            let buffer_state = buffer_state.clone();
+            move |ab: TypedArray<u8>| {
+                let data = ab.to_vec();
+                if data.len() > 0 {
+                    *buffer_state.borrow_mut() = BufferState::Buffer(data);
+                }
+            }
+        };
+
+        let on_error = {
+            let buffer_state = buffer_state.clone();
+            move |s: String| {
+                let msg = format!("Fail to read file from web {}", s);
+                *buffer_state.borrow_mut() = BufferState::Error(msg);
+            }
         };
 
         js!{
@@ -24,28 +46,53 @@ impl FileSystem {
             oReq.open("GET", filename, true);
             oReq.responseType = "arraybuffer";
 
+            var on_error_js = function(s){
+                var on_error = @{on_error};
+                on_error(s);
+                on_error.drop();
+            };
+
             oReq.onload = function (oEvent) {
-                var get_buffer = @{get_buffer};
+                var status = oReq.status;
                 var arrayBuffer = oReq.response; // Note: not oReq.responseText
-                if (arrayBuffer) {
-                    get_buffer(new Uint8Array(arrayBuffer));
+                if (status == 200 && arrayBuffer) {
+                    var on_get_buffer = @{on_get_buffer};
+                    on_get_buffer(new Uint8Array(arrayBuffer));
+                    on_get_buffer.drop();
+                } else {
+                    on_error_js("Fail to get array buffer from network..");
                 }
-                get_buffer.drop();
+            };
+
+            oReq.onerror = function(oEvent) {
+                on_error_js("Fail to read from network..");
             };
 
             oReq.send(null);
         }
 
-        Ok(File { buffer: buffer })
+        Ok(File {
+            buffer_state: buffer_state,
+        })
     }
 }
 
 impl File {
     pub fn is_ready(&self) -> bool {
-        self.buffer.borrow().len() > 0
+        let bs = self.buffer_state.borrow();
+        match *bs {
+            BufferState::Empty => false,
+            BufferState::Error(_) => true,
+            BufferState::Buffer(_) => true,
+        }
     }
 
     pub fn read_binary(&mut self) -> Result<Vec<u8>, IoError> {
-        Ok(self.buffer.borrow().clone())
+        let bs = self.buffer_state.borrow();
+        match *bs {
+            BufferState::Error(ref s) => Err(std::io::Error::new(ErrorKind::Other, s.clone())),
+            BufferState::Buffer(ref v) => Ok(v.clone()),
+            _ => unreachable!(),
+        }
     }
 }

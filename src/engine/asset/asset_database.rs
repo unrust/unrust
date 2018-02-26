e7a7875ae5989008a9f4cc6361f7863ecf6e75ab
Edwin Cheng <edwin0cheng@gmail.com>
2018-02-27 03:07:36 +0800

    Refactor out Resource to new file.

--- src/engine/asset/asset_database.rs
+++ src/engine/asset/asset_database.rs
@@ -1,119 +1,19 @@
 use std::rc::Rc;
-use std::cell::{Ref, RefCell};
+use std::cell::RefCell;
 use std::collections::HashMap;
 
 use engine::asset::{CubeMesh, PlaneMesh, Quad};
 use engine::asset::default_font_bitmap::DEFAULT_FONT_DATA;
 use engine::asset::fs;
 use engine::asset::loader;
+use engine::asset::Resource;
 
 use engine::{MeshBuffer, ShaderFs, ShaderProgram, ShaderVs, Texture, TextureFiltering};
-use futures::{Async, Future};
-use std::mem;
 use std::fmt::Debug;
-use std::fmt;
 use std::ops::Deref;
 
 use image;
 use image::ImageBuffer;
-
-impl<T> Debug for ResourceKind<T>
-where
-    T: Debug + loader::Loadable,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            &ResourceKind::Consumed => write!(f, "ResourceKind::Consumed"),
-            &ResourceKind::Data(ref t) => write!(f, "ResourceKind::Data({:?})", *t),
-            &ResourceKind::Future(_) => write!(f, "ResourceKind::Future"),
-        }
-    }
-}
-
-enum ResourceKind<T: Debug> {
-    Consumed,
-    Data(T),
-    Future(Box<Future<Item = T, Error = AssetError>>),
-}
-
-impl<T: Debug> ResourceKind<T> {
-    fn try_into_data(self) -> Option<T> {
-        match self {
-            ResourceKind::Data(d) => Some(d),
-            _ => None,
-        }
-    }
-
-    fn try_as_data(&self) -> Option<&T> {
-        match self {
-            &ResourceKind::Data(ref d) => Some(d),
-            _ => None,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Resource<T: Debug + loader::Loadable>(RefCell<ResourceKind<T>>);
-
-impl<T: Debug + loader::Loadable> Resource<T> {
-    pub fn new_future<FT>(f: FT) -> Self
-    where
-        FT: Future<Item = T, Error = AssetError> + 'static,
-    {
-        Resource(RefCell::new(ResourceKind::Future(Box::new(f))))
-    }
-
-    pub fn new(f: T) -> Self {
-        Resource(RefCell::new(ResourceKind::Data(f)))
-    }
-
-    pub fn try_into(&self) -> Result<T, AssetError> {
-        match &mut *self.0.borrow_mut() {
-            &mut ResourceKind::Future(ref mut f) => {
-                return match f.poll() {
-                    Err(e) => Err(e),
-                    Ok(Async::NotReady) => Err(AssetError::NotReady),
-                    Ok(Async::Ready(i)) => Ok(i),
-                };
-            }
-
-            img @ &mut ResourceKind::Data(_) => {
-                let r = mem::replace(img, ResourceKind::Consumed);
-                Ok(r.try_into_data().unwrap())
-            }
-
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn try_borrow(&self) -> Result<Ref<T>, AssetError> {
-        let mut data = None;
-
-        if let &mut ResourceKind::Future(ref mut f) = &mut *self.0.borrow_mut() {
-            match f.poll() {
-                Err(e) => return Err(e),
-                Ok(Async::NotReady) => return Err(AssetError::NotReady),
-                Ok(Async::Ready(i)) => {
-                    data = Some(i);
-                }
-            }
-        }
-
-        if let Some(i) = data {
-            let kind: &mut ResourceKind<T> = &mut self.0.borrow_mut();
-            mem::replace(kind, ResourceKind::Data(i));
-        }
-
-        let b0 = self.0.borrow();
-        return Ok(Ref::map(b0, |t| t.try_as_data().unwrap()));
-    }
-}
-
-impl<T: Debug + loader::Loadable> From<T> for Resource<T> {
-    fn from(r: T) -> Resource<T> {
-        Resource::new(r)
-    }
-}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum AssetError {
--- src/engine/asset/mod.rs
+++ src/engine/asset/mod.rs
@@ -3,10 +3,12 @@ mod default_font_bitmap;
 mod quad;
 mod fs;
 mod primitives;
+mod resource;
 
 pub mod loader;
 pub use self::primitives::{CubeMesh, PlaneMesh};
 pub use self::quad::Quad;
-pub use self::asset_database::{Asset, AssetDatabase, AssetError, AssetSystem, LoadableAsset,
-                               Resource};
+pub use self::asset_database::{Asset, AssetDatabase, AssetError, AssetSystem, LoadableAsset};
+
+pub use self::resource::Resource;
 pub use self::fs::*;

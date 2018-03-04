715bafa99f9511135858dc148cfe18168389db2e
Edwin Cheng <edwin0cheng@gmail.com>
2018-03-04 12:45:25 +0800

    Added scenenodes example

---
+++ examples/scenenodes.rs
@@ -0,0 +1,151 @@
+extern crate unrust;
+
+use unrust::world::{Actor, World, WorldBuilder};
+use unrust::engine::{Directional, GameObject, Light, Material, Mesh};
+use unrust::world::events::*;
+use unrust::math::*;
+
+// GUI
+use unrust::imgui;
+
+pub struct MainScene {
+    eye: Vector3<f32>,
+    last_event: Option<AppEvent>,
+}
+
+// Actor is a trait object which would act like an component
+// (Because Box<Actor> implemented ComponentBased)
+impl Actor for MainScene {
+    fn new() -> Box<Actor> {
+        Box::new(MainScene {
+            eye: Vector3::new(-15.0, 15.0, -15.0),
+            last_event: None,
+        })
+    }
+
+    fn start(&mut self, _go: &mut GameObject, world: &mut World) {
+        // add direction light to scene.
+        {
+            let go = world.new_game_object();
+            go.borrow_mut()
+                .add_component(Light::new(Directional::default()));
+        }
+
+        // Added an cemter cube in the scene
+        let center = world.new_game_object();
+        center.borrow_mut().add_component(Cube::new());
+
+        for _ in 0..5 {
+            let cube = world.new_game_object();
+            let mut cube_mut = cube.borrow_mut();
+
+            cube_mut.add_component(Cube::new());
+            center.borrow_mut().add_child(&cube_mut);
+
+            cube_mut.transform.translation.vector = Vector3::new(0.0, 10.0, 0.0);
+        }
+    }
+
+    fn update(&mut self, _go: &mut GameObject, world: &mut World) {
+        // Handle Events
+        {
+            let target = Vector3::new(0.0, 0.0, 0.0);
+            let front = (self.eye - target).normalize();
+            let up = Vector3::y();
+
+            let mut reset = false;
+
+            for evt in world.events().iter() {
+                self.last_event = Some(evt.clone());
+                match evt {
+                    &AppEvent::KeyDown(ref key) => {
+                        match key.code.as_str() {
+                            "KeyA" => self.eye = Rotation3::new(up * -0.02) * self.eye,
+                            "KeyD" => self.eye = Rotation3::new(up * 0.02) * self.eye,
+                            "KeyW" => self.eye -= front * 2.0,
+                            "KeyS" => self.eye += front * 2.0,
+                            "Escape" => reset = true,
+                            _ => (),
+                        };
+                    }
+
+                    _ => (),
+                }
+            }
+
+            if reset {
+                world.reset();
+                // Because reset will remove all objects in the world,
+                // included this Actor itself
+                // so will need to add it back.
+                let scene = world.new_game_object();
+                scene.borrow_mut().add_component(MainScene::new());
+            }
+        }
+
+        // Update Camera
+        {
+            let mut cam = world.engine().main_camera.as_ref().unwrap().borrow_mut();
+
+            cam.lookat(
+                &Point3::from_coordinates(self.eye),
+                &Point3::new(0.0, 0.0, 0.0),
+                &Vector3::new(0.0, 1.0, 0.0),
+            );
+        }
+
+        // GUI
+        use imgui::Metric::*;
+
+        imgui::pivot((1.0, 1.0));
+        imgui::label(
+            Native(1.0, 1.0) - Pixel(8.0, 8.0),
+            "[WASD] : control camera\n[Esc]  : reload all (include assets)",
+        );
+
+        imgui::pivot((1.0, 0.0));
+        imgui::label(
+            Native(1.0, 0.0) + Pixel(-8.0, 8.0),
+            &format!("last event: {:?}", self.last_event),
+        );
+    }
+}
+
+pub struct Cube {}
+
+impl Actor for Cube {
+    fn new() -> Box<Actor> {
+        Box::new(Cube {})
+    }
+
+    fn start(&mut self, go: &mut GameObject, world: &mut World) {
+        let db = &mut world.asset_system();
+
+        let mut material = Material::new(db.new_program("phong"));
+        material.set("uMaterial.diffuse", db.new_texture("tex_a.png"));
+        material.set("uMaterial.shininess", 32.0);
+
+        let mut mesh = Mesh::new();
+        mesh.add_surface(db.new_mesh_buffer("cube"), material);
+        go.add_component(mesh);
+    }
+
+    fn update(&mut self, go: &mut GameObject, _world: &mut World) {
+        go.transform
+            .append_rotation_wrt_center_mut(&UnitQuaternion::new(Vector3::new(0.01, 0.0, 0.0)));
+    }
+}
+
+pub fn main() {
+    let mut world = WorldBuilder::new("Scene Nodes demo")
+        .with_size((640, 480))
+        .with_stats(true)
+        .build();
+
+    // Add the main scene as component of scene game object
+    let scene = world.new_game_object();
+    scene.borrow_mut().add_component(MainScene::new());
+    drop(scene);
+
+    world.event_loop();
+}

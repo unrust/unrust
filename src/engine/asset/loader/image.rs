aade3dccf7be30630e3cbe7232f3486aa7b38183
Edwin Cheng <edwin0cheng@gmail.com>
2018-03-08 04:27:36 +0800

    Refactoring image loader

--- src/engine/asset/loader/image.rs
+++ src/engine/asset/loader/image.rs
@@ -8,6 +8,7 @@ use uni_app;
 
 use futures::prelude::*;
 use std::io;
+use std::fmt::Debug;
 
 pub struct ImageLoader {}
 
@@ -20,11 +21,18 @@ impl Loader<RgbaImage> for ImageLoader {
     }
 }
 
+fn make_invalid_format<E: Debug>(info: &ImageFileInfo, e: E) -> AssetError {
+    AssetError::InvalidFormat {
+        path: info.file_name.clone(),
+        len: info.orig_len,
+        reason: format!("{:?}", e),
+    }
+}
+
 struct ImageBufferLineSteam<T> {
     codec: T,
     ctx: ImageContext,
     read_bytes: usize,
-    row_len: usize,
 }
 
 // the number of bytes read for yield
@@ -38,40 +46,42 @@ where
     type Error = AssetError;
 
     fn poll(&mut self) -> Result<Async<Option<Self::Item>>, Self::Error> {
+        // The main magic is in here
+        // We just return an Async::NotReady to 'yield' the execution
         if self.read_bytes > READ_BYTES_YIELD_COUNT {
             self.read_bytes -= READ_BYTES_YIELD_COUNT;
             return Ok(Async::NotReady);
         }
 
-        let mut buffer = vec![0; self.row_len];
-        self.read_bytes += self.row_len;
+        let mut buffer = vec![0; self.ctx.row_len];
+        self.read_bytes += self.ctx.row_len;
 
         match self.codec.read_scanline(&mut buffer) {
             Err(image::ImageError::ImageEnd) => Ok(Async::Ready(None)),
             Ok(_) => Ok(Async::Ready(Some(buffer))),
-            Err(e) => Err(AssetError::InvalidFormat {
-                path: self.ctx.file_name.clone(),
-                len: self.ctx.orig_len,
-                reason: format!("{:?}", e),
-            }),
+            Err(e) => Err(make_invalid_format(&self.ctx.info, e)),
         }
     }
 }
 
-fn image_line_stream<T>(
+fn image_rows_stream<T>(
     codec: T,
     ctx: ImageContext,
-    row_len: usize,
 ) -> Box<Stream<Item = Vec<u8>, Error = AssetError>>
 where
     T: image::ImageDecoder + 'static,
 {
     Box::new(ImageBufferLineSteam {
         codec: codec,
-        ctx: ctx,
         read_bytes: 0,
-        row_len: row_len,
+        ctx: ctx,
     })
+}
+
+#[derive(Clone)]
+struct ImageFileInfo {
+    file_name: String,
+    orig_len: usize,
 }
 
 #[derive(Clone)]
@@ -79,8 +89,35 @@ struct ImageContext {
     w: u32,
     h: u32,
     color: image::ColorType,
-    file_name: String,
-    orig_len: usize,
+    info: ImageFileInfo,
+    row_len: usize,
+}
+
+fn new_img_context(
+    buf: Vec<u8>,
+    info: ImageFileInfo,
+) -> Result<(ImageContext, png::PNGDecoder<io::Cursor<Vec<u8>>>), image::ImageError> {
+    let format = image::guess_format(&buf)?;
+
+    // TODO: support other format
+    assert!(format == image::ImageFormat::PNG);
+
+    let mut codec = png::PNGDecoder::new(io::Cursor::new(buf));
+
+    let color = codec.colortype()?;
+    let (w, h) = codec.dimensions()?;
+    let row_len = codec.row_len()?;
+
+    Ok((
+        ImageContext {
+            w,
+            h,
+            color,
+            row_len,
+            info,
+        },
+        codec,
+    ))
 }
 
 impl Loadable for RgbaImage {
@@ -96,107 +133,70 @@ impl Loadable for RgbaImage {
                 Ok(ref mut file) => {
                     let buf = file.read_binary()
                         .map_err(|_| AssetError::ReadBufferFail(file.name()))?;
-
                     Ok((buf, file.name()))
                 }
-
                 Err(e) => Err(AssetError::FileIoError(e)),
             })
         };
 
         let decoded = img_buf.and_then(|(whole_buf, file_name)| {
-            let orig_len = whole_buf.len();
-
-            let format = image::guess_format(&whole_buf).map_err(|e| AssetError::InvalidFormat {
-                path: file_name.clone(),
-                len: orig_len,
-                reason: format!("{:?}", e),
-            })?;
-
-            // TODO: support other format
-            assert!(format == image::ImageFormat::PNG);
-
-            let mut codec = png::PNGDecoder::new(io::Cursor::new(whole_buf));
-
-            let color = codec.colortype().map_err(|e| AssetError::InvalidFormat {
-                path: file_name.clone(),
-                len: orig_len,
-                reason: format!("{:?}", e),
-            })?;
-
-            let (w, h) = codec.dimensions().map_err(|e| AssetError::InvalidFormat {
-                path: file_name.clone(),
-                len: orig_len,
-                reason: format!("{:?}", e),
-            })?;
-
-            let ctx = ImageContext {
-                w,
-                h,
-                color,
-                orig_len,
-                file_name: file_name.clone(),
+            let info = ImageFileInfo {
+                file_name,
+                orig_len: whole_buf.len(),
             };
 
-            let row_len = codec.row_len().map_err(|e| AssetError::InvalidFormat {
-                path: ctx.file_name.clone(),
-                len: ctx.orig_len,
-                reason: format!("{:?}", e),
-            })?;
+            let (ctx, codec) =
+                new_img_context(whole_buf, info.clone()).map_err(|e| AssetError::InvalidFormat {
+                    path: info.file_name.clone(),
+                    len: info.orig_len,
+                    reason: format!("{:?}", e),
+                })?;
 
-            let stream = image_line_stream(codec, ctx.clone(), row_len);
+            let stream = image_rows_stream(codec, ctx.clone());
             let buff_future = stream.fold(Vec::new(), |mut acc, mut buffer| {
                 acc.append(&mut buffer);
                 Ok(acc)
             });
 
-            Ok(buff_future.map(move |buf| match color {
+            Ok(buff_future.map(move |buf| match ctx.color {
                 image::ColorType::RGBA(8) | image::ColorType::RGB(8) => Ok((buf, ctx)),
-                _ => Err(AssetError::InvalidFormat {
-                    path: file_name.clone(),
-                    len: orig_len,
-                    reason: format!("{:?}", image::ImageError::UnsupportedColor(color)),
-                }),
+                _ => Err(make_invalid_format(
+                    &ctx.info,
+                    image::ImageError::UnsupportedColor(ctx.color),
+                )),
             }))
         });
 
-        let decoded = decoded.flatten();
+        let decoded = decoded.flatten().and_then(|r| r);
 
-        let img = decoded.and_then(move |r| match r {
-            Ok((decoded, ctx)) => {
-                let t = uni_app::now();
+        let img = decoded.and_then(move |(decoded, ctx)| {
+            let t = uni_app::now();
 
-                let img = match ctx.color {
-                    image::ColorType::RGBA(_) => {
-                        image::ImageBuffer::from_raw(ctx.w, ctx.h, decoded)
-                            .map(image::DynamicImage::ImageRgba8)
-                    }
-                    image::ColorType::RGB(_) => image::ImageBuffer::from_raw(ctx.w, ctx.h, decoded)
-                        .map(image::DynamicImage::ImageRgb8),
-                    _ => unreachable!(),
-                };
+            let img = match ctx.color {
+                image::ColorType::RGBA(_) => image::ImageBuffer::from_raw(ctx.w, ctx.h, decoded)
+                    .map(image::DynamicImage::ImageRgba8),
+                image::ColorType::RGB(_) => image::ImageBuffer::from_raw(ctx.w, ctx.h, decoded)
+                    .map(image::DynamicImage::ImageRgb8),
+                _ => unreachable!(),
+            };
 
-                match img {
-                    Some(img) => {
-                        let rgba = img.to_rgba();
+            match img {
+                Some(img) => {
+                    let rgba = img.to_rgba();
+                    uni_app::App::print(format!(
+                        "image {} loading time : {}\n",
+                        &ctx.info.file_name,
+                        (uni_app::now() - t)
+                    ));
 
-                        uni_app::App::print(format!(
-                            "image {} loading time : {}\n",
-                            &ctx.file_name,
-                            (uni_app::now() - t)
-                        ));
-
-                        Ok(rgba)
-                    }
-
-                    None => Err(AssetError::InvalidFormat {
-                        path: ctx.file_name.clone(),
-                        len: ctx.orig_len,
-                        reason: format!("{:?}", image::ImageError::DimensionError),
-                    }),
+                    Ok(rgba)
                 }
+
+                None => Err(make_invalid_format(
+                    &ctx.info,
+                    image::ImageError::DimensionError,
+                )),
             }
-            Err(e) => Err(e),
         });
 
         // futurize

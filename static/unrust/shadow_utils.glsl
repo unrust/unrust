3b60a403922c52e14e67c7068dadb81009998b27
Edwin Cheng <edwin0cheng@gmail.com>
2018-04-03 11:36:10 +0800

    Use TexelSize for shadow map

--- static/unrust/shadow_utils.glsl
+++ static/unrust/shadow_utils.glsl
@@ -33,9 +33,9 @@ float ShadowCalculation(vec3 worldPos, vec3 normal, vec3 lightDir)
     // transform ndc to range [0,1]
     projCoords = projCoords * 0.5 + 0.5;    
     float currentDepth = projCoords.z;
-
-    float bias = max(0.005 * (1.0 - dot(normal, lightDir)), 0.001);
     vec2 texelSize =  1.0 / uShadowMap[index].map_size;
+
+    float bias = max(texelSize.x * 5.0 * (1.0 - dot(normal, lightDir)), texelSize.x);
 
     float shadow = 0.0;
     for(int x = -1; x <= 1; ++x)
